a495042a87616f7a64071308ffa2d5e9c6fbbfa8
connie <joshg7946@gmail.com>
2025-05-20 13:47:47 -0500

    made first real level
    "love level" created for my partner
    first use of intro dialogue

--- scripts/master/dialogue_controller.gd
+++ scripts/master/dialogue_controller.gd
@@ -1,5 +1,7 @@
 extends Node
 
+@onready var dialogueUI = $"../CanvasLayer/DialogueUI"
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
