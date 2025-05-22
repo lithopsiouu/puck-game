67b7a32ed53f5895dcd0fc308d99386e73115a8d
connie <joshg7946@gmail.com>
2025-05-21 21:51:37 -0500

    win screen update
    added elapsed time timer
    UI shows elapsed time in non-timed modes
    first level added

--- UI/objective_ui.gd
+++ UI/objective_ui.gd
@@ -1,10 +1,19 @@
 extends Control
 
-@onready var gemLabel = $GemObjective/Panel/GemsLabel
-@onready var timerMins = $TimeObjective/Panel/TimerMins
-@onready var timerSecs = $TimeObjective/Panel/TimerSecs
-@onready var timerMsecs = $TimeObjective/Panel/TimerMilisecs
+@onready var timerMinsDown = $TimeObjective/Countdown/TimerMins
+@onready var timerSecsDown = $TimeObjective/Countdown/TimerSecs
+@onready var timerMsecsDown = $TimeObjective/Countdown/TimerMilisecs
+
+@onready var timerMinsUp = $Elapsed/TimerMins
+@onready var timerSecsUp = $Elapsed/TimerSecs
+@onready var timerMsecsUp = $Elapsed/TimerMilisecs
+
 @onready var timer: Timer = $TimeObjective/Timer
+
+@onready var timeLeftBox : Panel = $TimeObjective/Countdown
+@onready var timeElapsedBox : Panel = $Elapsed
+
+@onready var gemLabel = $GemObjective/Panel/GemsLabel
 @onready var timerBorder: Panel = $TimeObjective/TimerBorder
 @onready var gemsBorder: Panel = $GemObjective/Panel/GemsBorder
 var gamemode: int = 0
@@ -13,6 +22,9 @@ var smashedGems: int = 0
 # 0 = exit, 1 = gem, 2 = time
 
 @onready var time: float = timer.time_left
+var elapsedTime: float = 0
+var time_at_start: float = 0
+var time_now: float = 0
 var minutes: int = 0
 var seconds: int = 0
 var msec: int = 0
@@ -22,8 +34,10 @@ func _ready() -> void:
 	EventController.connect("gem_smashed", on_event_gem_smashed)
 	EventController.connect("send_gamemode", recieve_gamemode)
 	EventController.connect("send_timer", set_timer)
+	EventController.connect("get_timer", get_timer)
 	EventController.connect("game_win", win_process)
 	EventController.connect("send_total_gems", setup_gems_label)
+	time_at_start = Time.get_unix_time_from_system()
 
 func _physics_process(_delta: float) -> void:
 	#print(timer.time_left)
@@ -33,9 +47,19 @@ func _physics_process(_delta: float) -> void:
 		seconds = fmod(time, 60)
 		minutes = fmod(time, 3600) / 60
 		
-		timerMins.text = "| %02d" % minutes
-		timerSecs.text = ":%02d" % seconds
-		timerMsecs.text = ".%03d" % msec
+		timerMinsDown.text = "| %02d" % minutes
+		timerSecsDown.text = ":%02d" % seconds
+		timerMsecsDown.text = ".%03d" % msec
+	else:
+		time_now = Time.get_unix_time_from_system()
+		var time_elapsed = time_now - time_at_start
+		msec = fmod(time_elapsed, 1) * 100
+		seconds = fmod(time_elapsed, 60)
+		minutes = fmod(time_elapsed, 3600) / 60
+		
+		timerMinsUp.text = "| %02d" % minutes
+		timerSecsUp.text = ":%02d" % seconds
+		timerMsecsUp.text = ".%03d" % msec
 
 func win_process() -> void:
 	timer.paused = true
@@ -54,11 +78,14 @@ func win_process() -> void:
 func get_time_formatted() -> String:
 	return "%02d:%02d.%03d" % [minutes, seconds, msec]
 
-func set_timer(time: int):
-	print(str(time))
-	timer.wait_time = time
+func set_timer(funcTime: int):
+	print(str(funcTime))
+	timer.wait_time = funcTime
 	print(str(timer.wait_time))
 	timer.start()
+
+func get_timer():
+	return timer.time_left
 
 func on_event_gem_smashed() -> void:
 	smashedGems += 1
@@ -68,9 +95,14 @@ func on_event_gem_smashed() -> void:
 func recieve_gamemode(mode: int) -> void:
 	gamemode = mode
 	match mode:
-		0: $ExitObjective.visible = true
-		1: $GemObjective.visible = true
-		2: $TimeObjective.visible = true
+		0: 
+			$ExitObjective.visible = true
+			timeElapsedBox.visible = true
+		1: 
+			$GemObjective.visible = true
+			timeElapsedBox.visible = true
+		2: 
+			$TimeObjective.visible = true
 
 func setup_gems_label(allGems: int) -> void:
 	totalGems = allGems
