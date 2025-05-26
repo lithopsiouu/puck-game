20a260f58ef0bb8956cedb8656b9bab267b79c80
connie <joshg7946@gmail.com>
2025-05-26 18:47:18 -0500

    scalable level change
    level change now incrementally progresses from any valid level
    moved game levels to the levels folder
    added main menu screen

--- UI/nextLevelButton.gd
+++ UI/nextLevelButton.gd
@@ -1,4 +1,19 @@
 extends Button
 
+var levelPathPlaceholder: String = "res://scenes/Levels/lvl%s.tscn"
+var currentLevelPath: String
+var nextLevelPath: String
+
+func _ready() -> void:
+	_get_next_level()
+
 func _on_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/lvl2.tscn")
+	get_tree().change_scene_to_file(nextLevelPath)
+
+# Grabs level number and adds one to it and turns the new level number back into a filepath string
+func _get_next_level() -> void:
+	currentLevelPath = str(get_tree().get_current_scene().scene_file_path)
+	var levelInt = currentLevelPath.to_int()
+	nextLevelPath = levelPathPlaceholder % str(levelInt + 1)
+	if !ResourceLoader.exists(nextLevelPath):
+		nextLevelPath = "res://scenes/mainMenu.tscn"
