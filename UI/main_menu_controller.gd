7d49172a569e00584fff68d4ec9592e4b0c2d4a7
connie <joshg7946@gmail.com>
2025-05-27 12:23:11 -0500

    credits and main menu UI updates
    beautified level select
    added pretty credits menu
    created gorgeous menu to level transition

--- UI/main_menu_controller.gd
+++ UI/main_menu_controller.gd
@@ -4,6 +4,7 @@ extends Control
 @onready var settingsButtons = $SettingsButtons
 @onready var levelSelect = $LevelSelect
 @onready var mainButtons = $MainButtons
+@onready var credits = $Credits
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -20,6 +21,10 @@ func _on_settings_button_pressed() -> void:
 	uiAnims.play("disk_enter")
 	mainButtons.visible = false
 
+func _on_credits_button_pressed() -> void:
+	credits.visible = true
+	uiAnims.play("credits_enter")
+
 
 func _on_exit_button_pressed() -> void:
 	#get_tree().root.propagate_notification(NOTIFICATION_WM_CLOSE_REQUEST)
@@ -30,11 +35,18 @@ func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	match anim_name:
 		"disk_enter":
 			settingsButtons.visible = true
+		"credits_enter":
+			#mainButtons.visible = false
+			pass
 		"disk_exit":
 			mainButtons.visible = true
 		"levelSelect_exit":
 			levelSelect.visible = false
 			mainButtons.visible = true
+		"credits_exit":
+			credits.visible = false
+		"all_disappear":
+			get_tree().change_scene_to_file("res://scenes/Levels/lvl1.tscn")
 
 func _on_settings_exit_button_pressed() -> void:
 	settingsButtons.visible = false
@@ -43,5 +55,12 @@ func _on_settings_exit_button_pressed() -> void:
 func _on_level_exit_button_pressed() -> void:
 	uiAnims.play("levelSelect_exit")
 
+func _on_credits_exit_button_pressed() -> void:
+	#mainButtons.visible = true
+	uiAnims.play("credits_exit")
+
 func _on_quick_play_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/Levels/lvl1.tscn")
+	uiAnims.play("all_disappear")
+
+func _on_level_select_button_pressed() -> void:
+	uiAnims.play("all_disappear")
